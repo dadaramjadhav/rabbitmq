2dff3249aa2e9189ae484749715f4afd40db347f
dm jadhav <dadaramjadhav@gmail.com>
2025-06-14 07:39:48 +0000

    code added

---
+++ quorum-example/quorum-example/src/main/java/com/example/rabbit_producer/Receiver.java
@@ -0,0 +1,13 @@
+package com.example.rabbit_producer;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Receiver {
+
+    // @RabbitListener(queues = "myqueue")
+    // public void receive(Customer customer) {
+    //     System.out.println("Received: " + customer);
+    // }
+}
